582aef9278e38207a0f0bb40db6f57483ac682de
Andres Felipe Castro <pipecar366@gmail.com>
2024-05-07 00:45:27 -0500

    Se agregaron todos los endpoints a la api gateway, aun no funcionan bien los de api health y los de api users no funcionan

--- API_GATEWAY/main.js
+++ API_GATEWAY/main.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const winston = require('winston');
 
 const app = express();
-const port = 8086;
+app.use(express.json());
+const port = process.env.PORT || 8086;
 
 // Configurar el logger
 const logger = winston.createLogger({
@@ -14,31 +15,212 @@ const logger = winston.createLogger({
         new winston.transports.Console(),
     ]
 });
-
-// Ruta para la operación de autenticación
-app.post('/auth', async (req, res) => {
+//auth
+app.post('/login', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.body)
     try {
-        const response = await axios.post('http://api_users/login', req.body);
-        logger.error('Error al reenviar la peticion de autenticacion a api_users', error);
-        res.json(response.data);
+        const response = await axios.post('http://api_users:8082/login', req.body)
+        logger.info('Peticion al loguearse de api_users reenviada');
+        res.send(response.data);
     } catch (error) {
-        logger.error('Error forwarding authentication request', error);
-        res.status(500).json({ error: 'Error al reenviar la peticion de autenticacion a api_users' });
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+
     }
 });
 
+//usuarios
 // Ruta para la operación de registro
-app.post('/register', async (req, res) => {
+app.post('/usuarios', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.headers)
+    console.log(req.body)
     try {
-        const response = await axios.post('http://api_users/register', req.body);
+        const response = await axios.post('http://api_users:8082/usuarios', req.body)
         logger.info('Peticion a registro de api_users reenviada');
-        res.json(response.data);
+        res.send(response.data);
     } catch (error) {
-        logger.error('Error al reenviar la peticion de registro a api_users', error);
-        res.status(500).json({ error: 'Error al reenviar la peticion de registro a api_users' });
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
     }
 });
+
+app.post('/usuarios/recuperarContraseña/:email', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.headers)
+    console.log(req.body)
+    try {
+        const email = req.params.email; // Obtener el valor del email de la ruta
+        const token = req.headers.authorization;
+        const response = await axios.post(`http://api_users:8082/usuarios/recuperarContraseña/${email})`, req.body)
+        logger.info('Peticion a recuperar contraseña de api_users reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+app.get('/usuarios/:id', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.headers)
+    console.log(req.body)
+    try {
+        const id = req.params.id; // Obtener el valor del id de la ruta
+        const token = req.headers.authorization;
+        const response = await axios.get(`http://api_users:8082/usuarios/${id}`, {
+            headers: {
+                Authorization: token
+            }
+        })
+        logger.info('Peticion a obtener usuario de api_users reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+
+app.put('/usuarios/:id', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.headers)
+    console.log(req.body)
+    try {
+        const id = req.params.id; // Obtener el valor del id de la ruta
+        const token = req.headers.authorization;
+        const response = await axios.put(`http://api_users:8082/usuarios/${id}`, req.body, {
+            headers: {
+                Authorization: token
+            }
+        })
+        logger.info('Peticion a actualizar usuario de api_users reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+app.put('/usuarios/actualizarContraseña/:id', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.headers)
+    console.log(req.body)
+    try {
+        const id = req.params.id; // Obtener el valor del id de la ruta
+        const response = await axios.put(`http://api_users:8082/usuarios/actualizarContraseña/${id}`, {
+            headers: {
+                Authorization: token
+            }
+        })
+        logger.info('Peticion a actualizar la contraseña de api_users reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.headers)
+    console.log(req.body)
+    try {
+        const id = req.params.id; // Obtener el valor del id de la ruta
+        const response = await axios.delete(`http://api_users:8082/usuarios/${id}`, {
+            headers: {
+                Authorization: token
+            }
+        })
+        logger.info('Peticion a eliminar de api_users reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+//api-logs
+
+app.post('/logs', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.body)
+    try {
+        const response = await axios.post('http://api_logs:8083/logs', req.body)
+        logger.info('Peticion a crear log de api_logs reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+app.get('/logs', async (req, res) => {
+    try {
+        const response = await axios.get(`http://api_logs:8083/logs`);
+        logger.info('Peticion a obtener logs de api_logs reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+app.get('/logs/:aplication', async (req, res) => {
+
+    try {
+        const aplication = req.params.aplication; // Obtener el valor de la aplicacion de la ruta
+        const response = await axios.get(`http://api_logs:8083/logs/${aplication}`);
+        logger.info('Peticion a obtener logs por aplicacion de api_logs reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+//api_health
+
+app.post('/register-service', async (req, res) => {
+    console.log("data de la peticion: ")
+    console.log(req.body)
+    try {
+        const response = await axios.post('http://api_health:8053/register', req.body);
+        logger.info('Peticion a registrar servicio de api_health reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
 app.get('/health', async (req, res) => {
+    try {
+        const response = await axios.get(`http://api_health:8053/health`);
+        logger.info('Peticion a health por de api_health reenviada');
+        res.send(response.data);
+    } catch (error) {
+        const errorMessage = error.response.data.message; // Extraer el mensaje de error
+        logger.error(errorMessage);
+        res.status(error.response.status).json({ error: errorMessage }); // Enviar solo el mensaje de error
+    }
+});
+
+
+//api gateway
+app.get('/health-apigateway', async (req, res) => {
     res.send("Hello world")
 });
 
